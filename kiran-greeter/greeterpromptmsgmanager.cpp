38b7e06ebb5267349fd9c21009ca5268733109df
liuxinhao <liuxinhao@kylinsec.com.cn>
2020-07-24 14:59:08 +0800

    feature(lightdm-kiran-greeter): lightdm greeter message queuing display to avoid message coverage
    - 加入Lightdm Greeter消息队列显示,每个消息显示至少满足设置的消息间隔,避免发生消息覆盖的情况
    
    Signed-off-by: liuxinhao <liuxinhao@kylinsec.com.cn>

---
+++ kiran-greeter/greeterpromptmsgmanager.cpp
@@ -0,0 +1,177 @@
+#include "greeterpromptmsgmanager.h"
+#include <QDebug>
+#include <QMetaType>
+#include <sys/sysinfo.h>
+#include <QMutexLocker>
+
+GreeterPromptMsgManager::GreeterPromptMsgManager(QLightDM::Greeter *greeter, QObject *parent)
+    :QThread (parent),
+      m_greeter(greeter)
+{
+    qRegisterMetaType<QLightDM::Greeter::PromptType>("QLightDM::Greeter::PromptType");
+    qRegisterMetaType<QLightDM::Greeter::MessageType>("QLightDM::Greeter::MessageType");
+    connect(m_greeter,
+            SIGNAL(showMessage(QString,QLightDM::Greeter::MessageType)),
+            this,
+            SLOT(addMsgToQueue(QString,QLightDM::Greeter::MessageType)));
+    connect(m_greeter,
+            SIGNAL(showPrompt(QString,QLightDM::Greeter::PromptType)),
+            this,
+            SLOT(addPromptToQueue(QString,QLightDM::Greeter::PromptType)));
+    connect(m_greeter,
+            SIGNAL(authenticationComplete()),
+            this,
+            SLOT(addAuthenticationCompleteToQueue()));
+}
+
+GreeterPromptMsgManager::~GreeterPromptMsgManager()
+{
+    m_sleepCancelCondition.wakeAll();
+    QThread::requestInterruption();
+    QThread::wait();
+}
+
+void GreeterPromptMsgManager::setMessageInterval(int ms)
+{
+    m_messageInterval = ms;
+}
+
+void GreeterPromptMsgManager::addMsgToQueue(QString text, QLightDM::Greeter::MessageType type)
+{
+    LightdmPromptMsg msg;
+
+    msg.type = LIGHTDM_MSG;
+    msg.info.msgType = type;
+    msg.text = text;
+
+    addMsgItemToQueue(msg);
+}
+
+void GreeterPromptMsgManager::addPromptToQueue(QString text, QLightDM::Greeter::PromptType type)
+{
+    LightdmPromptMsg msg;
+
+    msg.type = LIGHTDM_PROMPT;
+    msg.info.promptType = type;
+    msg.text = text;
+
+    addMsgItemToQueue(msg);
+}
+
+void GreeterPromptMsgManager::addAuthenticationCompleteToQueue()
+{
+    bool reAuthentication = false;
+
+    /// 如果认证失败,往队列中加入一个认证错误消息
+    if(!m_greeter->isAuthenticated()){
+        LightdmPromptMsg authFailedMsg;
+        authFailedMsg.type = LIGHTDM_MSG;
+        if(m_havePrompted){
+            if(!m_havePAMError){
+                authFailedMsg.text = "Incorrect password, please try again";
+            }
+            reAuthentication = true;
+        }else{
+            if(!m_havePAMError){
+                authFailedMsg.text = tr("Failed to authenticate");
+            }
+        }
+        if(!authFailedMsg.text.isEmpty())
+            addMsgItemToQueue(authFailedMsg);
+    }
+
+    LightdmPromptMsg msg;
+    msg.type = LIGHTDM_AUTHENTICATION_COMPLETE;
+    msg.info.authenticationCompleteResult.isSuccess = m_greeter->isAuthenticated();
+    msg.info.authenticationCompleteResult.reAuthentication = reAuthentication;
+    addMsgItemToQueue(msg);
+}
+
+void GreeterPromptMsgManager::reset()
+{
+    m_semaphore.acquire(m_semaphore.available());
+    m_msgQueue.clear();
+    m_sleepCancelCondition.wakeAll();
+    m_messageShowTime = 0;
+    m_havePAMError = false;
+    m_havePrompted = false;
+}
+
+void GreeterPromptMsgManager::addMsgItemToQueue(const GreeterPromptMsgManager::LightdmPromptMsg &msg)
+{
+    qInfo() <<"queue append: " << msg.type << msg.text;
+    m_semaphore.release();
+    m_msgQueue.enqueue(msg);
+}
+
+bool GreeterPromptMsgManager::getMsgItemFromQueue(GreeterPromptMsgManager::LightdmPromptMsg &msg,
+                                                         int ms)
+{
+    if( m_semaphore.tryAcquire(1,ms) ){
+        msg = m_msgQueue.dequeue();
+        qInfo() << "queue getItem: " << msg.type << msg.text;
+        return true;
+    }
+    return false;
+}
+
+qint64 GreeterPromptMsgManager::getUpTime()
+{
+    struct sysinfo sysInfo;
+    int res = 0;
+    qint64 uptime = 0;
+
+    res = sysinfo(&sysInfo);
+    if(res!=0){
+        qWarning("sysinfo get uptime failed,%s",strerror(res));
+        return uptime;
+    }
+
+    uptime = sysInfo.uptime;
+    return uptime;
+}
+
+void GreeterPromptMsgManager::run()
+{
+    forever {
+        /// 若线程被请求中断退出线程
+        if ( QThread::currentThread()->isInterruptionRequested() ) {
+            return;
+        }
+        LightdmPromptMsg msg;
+        if(getMsgItemFromQueue(msg,300)){
+            qint64 uptime = getUpTime();
+            /// 第一个prompt不做等待操作
+            /// 其他消息等待上一次Msg消息超过时间间隔再分发
+            if( !(msg.type==LIGHTDM_PROMPT&&!m_havePrompted)
+                    && ((m_messageShowTime+m_messageInterval) >= uptime) ){
+                QMutexLocker mutexLock(&m_sleepCancelMutex);
+                /// 等待取消条件
+                /// 如果取消条件满足,不分发该条Lightdm消息
+                /// 可能发生在切换切换用户认证或取消认证
+                if( m_sleepCancelCondition.wait(&m_sleepCancelMutex,
+                                            ((m_messageShowTime+m_messageInterval)-uptime)*1000) ){
+                    continue;
+                }
+            }
+            /// 分发消息
+            switch (msg.type) {
+            case LIGHTDM_MSG:
+                m_messageShowTime = uptime;
+                if(msg.info.msgType==QLightDM::Greeter::MessageType::MessageTypeError){
+                    m_havePAMError = true;
+                }
+                emit showMessage(msg.text,msg.info.msgType);
+                break;
+            case LIGHTDM_PROMPT:
+                m_havePrompted = true;
+                emit showPrompt(msg.text,msg.info.promptType);
+                break;
+            case LIGHTDM_AUTHENTICATION_COMPLETE:
+                emit authenticationComplete(msg.info.authenticationCompleteResult.isSuccess,
+                                            msg.info.authenticationCompleteResult.reAuthentication);
+                break;
+            }
+        }
+    }
+}
