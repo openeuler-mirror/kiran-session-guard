9ef55750a289a18fc7b2443ab736e0ef2f276fd2
liuxinhao <liuxinhao@kylinsec.com.cn>
2020-08-07 16:26:41 +0800

    fix(lightdm-kiran-greeter): fix onboard crash on exit
    - 修复onboard退出时崩溃
    
    Signed-off-by: liuxinhao <liuxinhao@kylinsec.com.cn>

--- kiran-greeter/greeterkeyboard.h
+++ kiran-greeter/greeterkeyboard.h
@@ -3,6 +3,7 @@
 
 #include <QObject>
 #include <QProcess>
+
 class GreeterKeyboard : public QObject
 {
     Q_OBJECT
@@ -14,13 +15,19 @@ public:
     void showAdjustSize(QWidget*parent=nullptr);
     void hide();
     QWidget* getKeyboard();
+    void resetParentAndTermProcess();
+public slots:
+    void slot_finished(int exitCode,QProcess::ExitStatus exitStatus);
 private:
     explicit GreeterKeyboard(QObject *parent = nullptr);
+protected:
+    bool eventFilter(QObject *obj, QEvent *event) override;
 signals:
 private slots:
     void slotReadyReadStandardOutput();
 private:
     QWidget* m_keyboardWidget;
+    QProcess* m_process;
 };
 
 #endif // GREETERKEYBOARD_H
