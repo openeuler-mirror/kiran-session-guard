695b1351e03eddeda09d57791369bf7d2cafadcc
liuxinhao <liuxinhao@kylinsec.com.cn>
2020-08-24 13:41:23 +0800

    feature(kiran-greeter-settings): update translate
    - 更新设置背景图失败翻译
    
    Signed-off-by: liuxinhao <liuxinhao@kylinsec.com.cn>

--- kiran-greeter-setting/translations/kiran-greeter-settings.zh_CN.ts
+++ kiran-greeter-setting/translations/kiran-greeter-settings.zh_CN.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_CN">
+<TS version="2.0" language="zh_CN">
 <context>
     <name>GreeterSetting</name>
     <message>
@@ -10,108 +10,118 @@
         <translation>登录设置</translation>
     </message>
     <message>
-        <location filename="../greetersetting.ui" line="198"/>
+        <location filename="../greetersetting.ui" line="152"/>
         <source>Appearance Setting</source>
         <translatorcomment>外观设置</translatorcomment>
         <translation>外观设置</translation>
     </message>
     <message>
-        <location filename="../greetersetting.ui" line="241"/>
+        <location filename="../greetersetting.ui" line="186"/>
         <source>Select Image</source>
         <translatorcomment>选择图片</translatorcomment>
         <translation>选择图片</translation>
     </message>
     <message>
-        <location filename="../greetersetting.ui" line="273"/>
+        <location filename="../greetersetting.ui" line="208"/>
         <source>Browse</source>
         <translation>浏览</translation>
     </message>
     <message>
-        <location filename="../greetersetting.ui" line="291"/>
+        <location filename="../greetersetting.ui" line="218"/>
         <source>Scale Mode</source>
         <translation>是否开启缩放</translation>
     </message>
     <message>
-        <location filename="../greetersetting.ui" line="763"/>
+        <location filename="../greetersetting.ui" line="536"/>
         <source>auto login account(take effect after restart)</source>
         <translation>自动登录用户名(重启后生效)</translation>
     </message>
     <message>
-        <location filename="../greetersetting.ui" line="812"/>
+        <location filename="../greetersetting.ui" line="568"/>
         <source>auto login delay(seconds)(take effect after restart)</source>
         <translation>自动登录延时(秒)(重启后生效)</translation>
     </message>
     <message>
         <source>auto login delay(seconds)</source>
-        <translation type="vanished">自动登录延时(秒)</translation>
+        <translation>自动登录延时(秒)</translation>
     </message>
     <message>
-        <location filename="../greetersetting.cpp" line="57"/>
+        <location filename="../greetersetting.cpp" line="70"/>
         <source>auto</source>
         <translation>自动</translation>
     </message>
     <message>
-        <location filename="../greetersetting.cpp" line="58"/>
+        <location filename="../greetersetting.cpp" line="71"/>
         <source>manual</source>
         <translation>手动</translation>
     </message>
     <message>
-        <location filename="../greetersetting.cpp" line="59"/>
+        <location filename="../greetersetting.cpp" line="72"/>
         <source>disable</source>
         <translation>禁用</translation>
     </message>
     <message>
-        <location filename="../greetersetting.ui" line="420"/>
+        <location filename="../greetersetting.ui" line="289"/>
         <source>Scale Factor</source>
         <translation>缩放比例</translation>
     </message>
     <message>
-        <location filename="../greetersetting.ui" line="550"/>
+        <location filename="../greetersetting.ui" line="362"/>
         <source>User Login</source>
         <translation>用户登录设置</translation>
     </message>
     <message>
-        <location filename="../greetersetting.ui" line="606"/>
+        <location filename="../greetersetting.ui" line="413"/>
         <source>enable manual login</source>
         <translation>允许手动登录</translation>
     </message>
     <message>
-        <location filename="../greetersetting.ui" line="699"/>
+        <location filename="../greetersetting.ui" line="490"/>
         <source>hide user list</source>
         <translation>隐藏用户列表</translation>
     </message>
     <message>
         <source>auto login account</source>
-        <translation type="vanished">自动登录用户名</translation>
+        <translation>自动登录用户名</translation>
     </message>
     <message>
         <source>auto login delay(ms)</source>
-        <translation type="vanished">自动登录延时(毫秒)</translation>
+        <translation>自动登录延时(毫秒)</translation>
     </message>
     <message>
-        <location filename="../greetersetting.cpp" line="25"/>
+        <location filename="../greetersetting.cpp" line="42"/>
         <source>greeter settings</source>
         <translation>登录设置</translation>
     </message>
     <message>
-        <location filename="../greetersetting.cpp" line="43"/>
+        <location filename="../greetersetting.cpp" line="58"/>
         <source>appearance</source>
         <translation>外观</translation>
     </message>
     <message>
-        <location filename="../greetersetting.cpp" line="49"/>
+        <location filename="../greetersetting.cpp" line="63"/>
         <source>user login</source>
         <translation>用户登录</translation>
     </message>
     <message>
-        <location filename="../greetersetting.cpp" line="82"/>
+        <location filename="../greetersetting.cpp" line="94"/>
         <source>select greeter background</source>
         <translation>选择登录背景</translation>
     </message>
     <message>
-        <location filename="../greetersetting.cpp" line="84"/>
+        <location filename="../greetersetting.cpp" line="96"/>
         <source>image files(*.bmp *.jpg *.png *.tif *.gif *.pcx *.tga *.exif *.fpx *.svg *.psd *.cdr *.pcd *.dxf *.ufo *.eps *.ai *.raw *.WMF *.webp)</source>
         <translation></translation>
+    </message>
+    <message>
+        <location filename="../greetersetting.cpp" line="110"/>
+        <source>warning</source>
+        <translation>警告</translation>
+    </message>
+    <message>
+        <location filename="../greetersetting.cpp" line="110"/>
+        <source>Failed to set background image.</source>
+        <translation>设置登录背景图片失败</translation>
     </message>
 </context>
 <context>
