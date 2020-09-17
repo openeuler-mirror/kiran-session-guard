857c93737ca820715e5ed018f1d8b1b63c010d2f
liuxinhao <liuxinhao@kylinsec.com.cn>
2020-09-17 11:47:34 +0800

    fix(lightdm-kiran-greeter): log files are truncated every time the log module is initialized
    - 每次日志模块初始化对日志文件进行截断
    
    Signed-off-by: liuxinhao <liuxinhao@kylinsec.com.cn>

--- public/log.cpp
+++ public/log.cpp
@@ -41,7 +41,7 @@ bool Log::init(QString filePath)
             fprintf(stderr,"make log file failed\n");
             return false;
         }
-        if( !file.open(QIODevice::WriteOnly|QIODevice::Append|QIODevice::Text) ){
+        if( !file.open(QIODevice::WriteOnly|QIODevice::Truncate|QIODevice::Text) ){
             return false;
         }
         file.close();
