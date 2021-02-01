1bbf17d3e94e983c2a9566ba8fae2693da8f89a5
liuxinhao <liuxinhao@kylinsec.com.cn>
2021-02-01 11:22:31 +0800

    feature(style): update the interface style and use the image selection control of kiranwidgets-qt5-2.1.1
    - 更新界面样式，使用KiranWidgets-qt5-2.1.1版本的图片选择控件
    
    Closes #30431

--- kiran-greeter-settings/translations/kiran-greeter-settings.zh_CN.ts
+++ kiran-greeter-settings/translations/kiran-greeter-settings.zh_CN.ts
@@ -4,139 +4,140 @@
 <context>
     <name>GreeterSettingWindow</name>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="48"/>
+        <location filename="../src/greeter-setting-window.cpp" line="47"/>
         <source>greeter settings</source>
         <translation>登录设置</translation>
     </message>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="75"/>
+        <location filename="../src/greeter-setting-window.cpp" line="74"/>
         <source>appearance</source>
         <translation>外观设置</translation>
     </message>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="79"/>
+        <location filename="../src/greeter-setting-window.cpp" line="78"/>
         <source>autologin</source>
         <translation>自动登录</translation>
     </message>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="134"/>
+        <location filename="../src/greeter-setting-window.cpp" line="133"/>
         <source>Automatic Logon</source>
         <translation>自动登录</translation>
     </message>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="143"/>
+        <location filename="../src/greeter-setting-window.cpp" line="142"/>
         <source>auto login account(take effect after restart)</source>
         <translation>自动登录用户名(重启后生效)</translation>
     </message>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="157"/>
+        <location filename="../src/greeter-setting-window.cpp" line="156"/>
         <source>auto login dealy(seconds)(take effect after restart)</source>
         <translation>自动登录延时(秒)(重启后生效)</translation>
     </message>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="190"/>
-        <location filename="../src/greeter-setting-window.cpp" line="397"/>
+        <location filename="../src/greeter-setting-window.cpp" line="189"/>
+        <location filename="../src/greeter-setting-window.cpp" line="396"/>
         <source>Save</source>
         <translation>保存</translation>
     </message>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="231"/>
+        <location filename="../src/greeter-setting-window.cpp" line="230"/>
         <source>Appearance Settings</source>
         <translation>外观设置</translation>
     </message>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="246"/>
+        <location filename="../src/greeter-setting-window.cpp" line="245"/>
         <source>Select Image</source>
         <translation>选择图片</translation>
     </message>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="253"/>
+        <location filename="../src/greeter-setting-window.cpp" line="252"/>
         <source>Browse</source>
         <translation>浏览</translation>
     </message>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="261"/>
+        <location filename="../src/greeter-setting-window.cpp" line="260"/>
         <source>select greeter background</source>
         <translation>选择登录界面背景</translation>
     </message>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="263"/>
+        <location filename="../src/greeter-setting-window.cpp" line="262"/>
         <source>image files(*.bmp *.jpg *.png *.tif *.gif *.pcx *.tga *.exif *.fpx *.svg *.psd *.cdr *.pcd *.dxf *.ufo *.eps *.ai *.raw *.WMF *.webp)</source>
         <translation>图片文件(*.bmp *.jpg *.png *.tif *.gif *.pcx *.tga *.exif *.fpx *.svg *.psd *.cdr *.pcd *.dxf *.ufo *.eps *.ai *.raw *.WMF *.webp)</translation>
     </message>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="271"/>
+        <location filename="../src/greeter-setting-window.cpp" line="270"/>
         <source>Warning</source>
         <translation>警告</translation>
     </message>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="272"/>
+        <location filename="../src/greeter-setting-window.cpp" line="271"/>
         <source>The login greeter may not be able to read the file normally. Do you want to continue?</source>
         <translation>登录界面可能不能正常读取该文件，您是否继续?</translation>
     </message>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="283"/>
+        <location filename="../src/greeter-setting-window.cpp" line="282"/>
         <source>Scale Mode</source>
         <translation>缩放模式</translation>
     </message>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="293"/>
+        <location filename="../src/greeter-setting-window.cpp" line="292"/>
         <source>auto</source>
         <translation>自动</translation>
     </message>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="294"/>
+        <location filename="../src/greeter-setting-window.cpp" line="293"/>
         <source>manual</source>
         <translation>手动</translation>
     </message>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="295"/>
+        <location filename="../src/greeter-setting-window.cpp" line="294"/>
         <source>disable</source>
         <translation>禁用</translation>
     </message>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="308"/>
+        <location filename="../src/greeter-setting-window.cpp" line="307"/>
         <source>Scale Factor</source>
         <translation>缩放比例</translation>
     </message>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="333"/>
+        <location filename="../src/greeter-setting-window.cpp" line="332"/>
         <source>Enable Manual Login</source>
         <translation>允许手动登录</translation>
     </message>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="358"/>
+        <location filename="../src/greeter-setting-window.cpp" line="357"/>
         <source>Hide User List</source>
         <translation>隐藏用户名</translation>
     </message>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="412"/>
+        <location filename="../src/greeter-setting-window.cpp" line="204"/>
+        <location filename="../src/greeter-setting-window.cpp" line="411"/>
         <source>Reset</source>
         <translation>重新加载</translation>
     </message>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="507"/>
-        <location filename="../src/greeter-setting-window.cpp" line="574"/>
+        <location filename="../src/greeter-setting-window.cpp" line="506"/>
+        <location filename="../src/greeter-setting-window.cpp" line="573"/>
         <source>Configuration changed</source>
         <translation>配置文件已被修改</translation>
     </message>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="508"/>
-        <location filename="../src/greeter-setting-window.cpp" line="575"/>
+        <location filename="../src/greeter-setting-window.cpp" line="507"/>
+        <location filename="../src/greeter-setting-window.cpp" line="574"/>
         <source>The external configuration file has changed
 If you choose to save, all external changes will be overwritten
 Select discard to discard the modification and reload the new configuration</source>
         <translation>外部配置文件已发生变化\n如果选择保存外部改变将会被覆盖\n选择丢弃将放弃所有更改，重新加载配置文件</translation>
     </message>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="555"/>
-        <location filename="../src/greeter-setting-window.cpp" line="608"/>
+        <location filename="../src/greeter-setting-window.cpp" line="554"/>
+        <location filename="../src/greeter-setting-window.cpp" line="607"/>
         <source>Save failed, reload</source>
         <translation>保存失败，已重新加载</translation>
     </message>
     <message>
-        <location filename="../src/greeter-setting-window.cpp" line="557"/>
-        <location filename="../src/greeter-setting-window.cpp" line="610"/>
+        <location filename="../src/greeter-setting-window.cpp" line="556"/>
+        <location filename="../src/greeter-setting-window.cpp" line="609"/>
         <source>Saved successfully</source>
         <translation>保存成功</translation>
     </message>
