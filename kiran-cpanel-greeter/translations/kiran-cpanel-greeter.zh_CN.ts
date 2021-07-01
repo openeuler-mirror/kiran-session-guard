c1d76d27a352af5ab1da6bd768cb6c8b4f20e1e3
liuxinhao <liuxinhao@kylinsec.com.cn>
2021-07-01 16:41:58 +0800

    feature(auth failed error msg): unified authentication failure prompt to "Failed to authenticate"
    - 统一认证失败提示消息为"认证失败"

--- kiran-cpanel-greeter/translations/kiran-cpanel-greeter.zh_CN.ts
+++ kiran-cpanel-greeter/translations/kiran-cpanel-greeter.zh_CN.ts
@@ -1,11 +1,10 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="zh_CN">
+<TS version="2.1" language="zh_CN">
 <context>
     <name>GreeterLineEdit</name>
     <message>
         <location filename="../../src/greeterlineedit.ui" line="32"/>
-        <location filename="../../build/lightdm-kiran-greeter_autogen/include/ui_greeterlineedit.h" line="90"/>
         <location filename="../../cmake-build-debug/lightdm-kiran-greeter_autogen/include/ui_greeterlineedit.h" line="90"/>
         <source>Form</source>
         <translation type="unfinished"></translation>
@@ -15,28 +14,24 @@
     <name>GreeterLoginWindow</name>
     <message>
         <location filename="../../src/greeterloginwindow.ui" line="23"/>
-        <location filename="../../build/lightdm-kiran-greeter_autogen/include/ui_greeterloginwindow.h" line="511"/>
         <location filename="../../cmake-build-debug/lightdm-kiran-greeter_autogen/include/ui_greeterloginwindow.h" line="511"/>
         <source>Form</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/greeterloginwindow.ui" line="390"/>
-        <location filename="../../build/lightdm-kiran-greeter_autogen/include/ui_greeterloginwindow.h" line="512"/>
         <location filename="../../cmake-build-debug/lightdm-kiran-greeter_autogen/include/ui_greeterloginwindow.h" line="512"/>
         <source>Name</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/greeterloginwindow.ui" line="568"/>
-        <location filename="../../build/lightdm-kiran-greeter_autogen/include/ui_greeterloginwindow.h" line="514"/>
         <location filename="../../cmake-build-debug/lightdm-kiran-greeter_autogen/include/ui_greeterloginwindow.h" line="514"/>
         <source>Recertification</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/greeterloginwindow.ui" line="665"/>
-        <location filename="../../build/lightdm-kiran-greeter_autogen/include/ui_greeterloginwindow.h" line="516"/>
         <location filename="../../cmake-build-debug/lightdm-kiran-greeter_autogen/include/ui_greeterloginwindow.h" line="516"/>
         <source>Not List</source>
         <translation type="unfinished"></translation>
@@ -82,48 +77,39 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/greeterloginwindow.cpp" line="354"/>
-        <source>Incorrect password, please try again</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../src/greeterloginwindow.cpp" line="357"/>
-        <source>Incorrect username or password</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../src/greeterloginwindow.cpp" line="370"/>
+        <location filename="../../src/greeterloginwindow.cpp" line="351"/>
+        <location filename="../../src/greeterloginwindow.cpp" line="362"/>
         <source>Failed to authenticate</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/greeterloginwindow.cpp" line="587"/>
+        <location filename="../../src/greeterloginwindow.cpp" line="579"/>
         <source>Not Listed?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/greeterloginwindow.cpp" line="636"/>
-        <location filename="../../src/greeterloginwindow.cpp" line="799"/>
+        <location filename="../../src/greeterloginwindow.cpp" line="628"/>
+        <location filename="../../src/greeterloginwindow.cpp" line="791"/>
         <source>Return</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/greeterloginwindow.cpp" line="648"/>
+        <location filename="../../src/greeterloginwindow.cpp" line="640"/>
         <source>Entry your name</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/greeterloginwindow.cpp" line="683"/>
+        <location filename="../../src/greeterloginwindow.cpp" line="675"/>
         <source>dddd MMM HH:mm</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/greeterloginwindow.cpp" line="853"/>
+        <location filename="../../src/greeterloginwindow.cpp" line="845"/>
         <source>Start face authentication</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/greeterloginwindow.cpp" line="864"/>
+        <location filename="../../src/greeterloginwindow.cpp" line="856"/>
         <source>Start fingerprint authentication</source>
         <translation type="unfinished"></translation>
     </message>
@@ -132,47 +118,47 @@
     <name>GreeterSetting</name>
     <message>
         <source>GreeterSetting</source>
-        <translation>登录设置</translation>
+        <translation type="vanished">登录设置</translation>
     </message>
     <message>
         <source>Appearance Setting</source>
-        <translation>外观设置</translation>
+        <translation type="vanished">外观设置</translation>
     </message>
     <message>
         <source>Select Image</source>
-        <translation>选择图片</translation>
+        <translation type="vanished">选择图片</translation>
     </message>
     <message>
         <source>Browse</source>
-        <translation>浏览</translation>
+        <translation type="vanished">浏览</translation>
     </message>
     <message>
         <source>Scale Mode</source>
-        <translation>缩放模式</translation>
+        <translation type="vanished">缩放模式</translation>
     </message>
     <message>
         <source>Scale Factor</source>
-        <translation>缩放比例</translation>
+        <translation type="vanished">缩放比例</translation>
     </message>
     <message>
         <source>User Login</source>
-        <translation>用户登录</translation>
+        <translation type="vanished">用户登录</translation>
     </message>
     <message>
         <source>enable manual login</source>
-        <translation>允许手动登录</translation>
+        <translation type="vanished">允许手动登录</translation>
     </message>
     <message>
         <source>hide user list</source>
-        <translation>隐藏用户列表</translation>
+        <translation type="vanished">隐藏用户列表</translation>
     </message>
     <message>
         <source>auto login account(take effect after restart)</source>
-        <translation type="unfinished">自动登录用户(重启后生效)</translation>
+        <translation type="obsolete">自动登录用户(重启后生效)</translation>
     </message>
     <message>
         <source>auto login delay(seconds)(take effect after restart)</source>
-        <translation>自动登录延时(秒)(重启后生效)</translation>
+        <translation type="vanished">自动登录延时(秒)(重启后生效)</translation>
     </message>
 </context>
 <context>
@@ -189,11 +175,11 @@
     </message>
     <message>
         <source>auto login account(take effect after restart)</source>
-        <translation>自动登录用户(重启后生效)</translation>
+        <translation type="vanished">自动登录用户(重启后生效)</translation>
     </message>
     <message>
         <source>auto login dealy(seconds)(take effect after restart)</source>
-        <translation>自动登录延时(重启后生效)</translation>
+        <translation type="vanished">自动登录延时(重启后生效)</translation>
     </message>
     <message>
         <location filename="../src/greeter-setting-window.cpp" line="127"/>
@@ -283,14 +269,12 @@ Select discard to discard the modification and reload the new configuration</sou
     <name>LoginButton</name>
     <message>
         <location filename="../../src/loginbutton.ui" line="14"/>
-        <location filename="../../build/lightdm-kiran-greeter_autogen/include/ui_loginbutton.h" line="71"/>
         <location filename="../../cmake-build-debug/lightdm-kiran-greeter_autogen/include/ui_loginbutton.h" line="71"/>
         <source>Form</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/loginbutton.ui" line="51"/>
-        <location filename="../../build/lightdm-kiran-greeter_autogen/include/ui_loginbutton.h" line="72"/>
         <location filename="../../cmake-build-debug/lightdm-kiran-greeter_autogen/include/ui_loginbutton.h" line="72"/>
         <source>TextLabel</source>
         <translation type="unfinished"></translation>
@@ -300,14 +284,12 @@ Select discard to discard the modification and reload the new configuration</sou
     <name>UserListItem</name>
     <message>
         <location filename="../../src/userlistitem.ui" line="17"/>
-        <location filename="../../build/lightdm-kiran-greeter_autogen/include/ui_userlistitem.h" line="90"/>
         <location filename="../../cmake-build-debug/lightdm-kiran-greeter_autogen/include/ui_userlistitem.h" line="90"/>
         <source>Form</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/userlistitem.ui" line="77"/>
-        <location filename="../../build/lightdm-kiran-greeter_autogen/include/ui_userlistitem.h" line="91"/>
         <location filename="../../cmake-build-debug/lightdm-kiran-greeter_autogen/include/ui_userlistitem.h" line="91"/>
         <source>Tom Hardy</source>
         <translation type="unfinished"></translation>
@@ -317,7 +299,6 @@ Select discard to discard the modification and reload the new configuration</sou
     <name>UserListWidget</name>
     <message>
         <location filename="../../src/userlistwidget.ui" line="23"/>
-        <location filename="../../build/lightdm-kiran-greeter_autogen/include/ui_userlistwidget.h" line="71"/>
         <location filename="../../cmake-build-debug/lightdm-kiran-greeter_autogen/include/ui_userlistwidget.h" line="71"/>
         <source>Form</source>
         <translation type="unfinished"></translation>
